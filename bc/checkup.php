0560ba23dd39bd9bc5bbe7d1cf73e26aef99aafb
PAPATIRI <syariftaufik11@gmail.com>
2020-11-02 21:52:27 +0700

    first commit from existing project

---
+++ bc/checkup.php
@@ -0,0 +1,179 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Checkup Covid-19</title>
+  <link rel="icon" href="img/icon.png">
+  <link rel="stylesheet" href="vendors/bootstrap/bootstrap.min.css">
+  <link rel="stylesheet" href="vendors/fontawesome/css/all.min.css">
+  <link rel="stylesheet" href="vendors/themify-icons/themify-icons.css">
+  <link rel="stylesheet" href="vendors/linericon/style.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.theme.default.min.css">
+  <link rel="stylesheet" href="vendors/owl-carousel/owl.carousel.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="vendors/aos/css/aos.css">
+</head>
+<body>
+  <!--================Header Menu Area =================-->
+  <header class="header_area">
+    <div class="main_menu">
+      <nav class="navbar navbar-expand-lg navbar-light">
+        <div class="container box_1620">
+          <!-- Brand and toggle get grouped for better mobile display -->
+          <a class="navbar-brand logo_h" href="index.php"><img src="img/logo.png" alt=""></a>
+          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <!-- Collect the nav links, forms, and other content for toggling -->
+         <div class="collapse navbar-collapse offset" id="navbarSupportedContent">
+            <ul class="nav navbar-nav menu_nav justify-content-end">
+              <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li> 
+              <li class="nav-item"><a class="nav-link" href="kasus.php">Kasus</a></li> 
+              <li class="nav-item"><a class="nav-link" href="rumahsakit.php">Rumahsakit Rujukan</a></li>
+              <li class="nav-item active"><a class="nav-link" href="checkup.php">Checkup!</a></li>
+            </ul>
+        </div>
+      </nav>
+    </div>
+  </header>
+  <!--================Header Menu Area =================-->
+  <main class="side-main">
+  <!--================ Hero sm Banner start =================-->      
+  <section class="hero-banner mb-10px">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-7" data-aos="fade-up"
+     data-aos-anchor-placement="top-bottom" data-aos-duration="20">
+            <div class="hero-banner__img">
+              <img class="img-fluid" width="500px" src="img/banner/self-checkup.png" alt="">
+            </div>
+          </div>
+          <div class="col-lg-5 pt-7" data-aos="fade-right">
+            <div class="hero-banner__content">
+              <h1>Salam, Indonesia Tangguh Melawan COVID-19</h1>
+              <p><font size="3px">Saat ini dengan mulai munculnya ODP, PDP dan kasus Positif COVID-19 di Indonesia, tentunya self-checkup COVID-19 akan dibutuhkan oleh masyarakat.</font></p>
+                <a class="button bg" href="checkup-pertanyaan.php" >Cek Diri Anda Sekarang!</a><br>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  <!--================ Hero sm Banner end =================-->
+  
+  <!--================ Pricing section start =================-->      
+   <section class="section-margin">
+    <div class="container">
+      <div class="section-intro pb-100px ">
+        <h2 class="section-intro__title text-center">Self Check up Covid-19</h2>
+        <p class="section-intro__subtitle text-left" ><font size="5"><p>Self assesment ini adalah alat sederhana yang bisa membantu menentukan apakah Anda sehat-sehat saja atau ada gejala yang memerlukan penilaian, pemeriksaan dan pengujian lebih lanjut untuk COVID-19.<br>
+
+Alat self assesment diadaptasi dari Self Assesment COVID-19 dari British Columbia, Kanada. Anda dapat menyelesaikan penilaian ini untuk diri sendiri maupun orang lain..</font></p>
+      </div>
+    </div>
+  </section>
+
+  
+  <!-- ================ start footer Area ================= -->
+  <footer class="footer-area" style="padding: 10px 0px 10px">
+
+  
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left mt-5">
+
+    <!-- Grid row -->
+    <div class="row mt-3">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-4 col-xl-3 mx-auto mb-4">
+
+        <!-- Content -->
+        <h6 class="text-uppercase font-weight-bold" style="color: white;">Hotline COVID-19<br>
+        Kemenkes RI </h6>
+        <h2> <i class="fas fa-phone mr-3"></i> Tlp. 199 ext 9 </h2>
+        <h6 class="text-uppercase font-weight-bold" style="color: white;">Website Mitigasi COVID-19 UMY </h6>
+        <h2> <a href="https://covid-19.umy.ac.id/">Covid-19.umy.ac.id</a></h2>
+
+      </div>
+      
+      <div class="col-sm-4 footer_widget hide_footer_widget">
+
+        <h4 class="widget_title">Kontak</h4>
+        <table>
+                <tr>
+                  <td width="120">Telp</td>
+                  <td>: +62 274 387656 (hunting)</td>
+                </tr>
+                <tr>
+                  <td>Faks</td>
+                  <td>: +62 274 387646</td>
+                </tr>
+                <tr>
+                  <td>Admisi</td>
+                                                                        <td>: +62 274 387649 </td>
+                                                                </tr>
+                <tr>
+                  <td>&nbsp;</td>
+                                                                        <td>&nbsp; +62 274 387656 Ext. 199 & 200 (hunting)</td>
+                </tr>
+                <tr>
+                  <td>Humas</td>
+                  <td>: +62 274 387656 Ext. 115</td>
+                </tr>
+                <tr>
+                  <td>Surel</td>
+                  <td>: bhp@umy.ac.id</td>
+                </tr>
+              </table>
+
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+       
+            <h4 class="widget_title">Alamat</h4>
+            <section  class="footer_widget_content">
+              Kampus Terpadu UMY<br>
+              Jl. Brawijaya,<br>
+              Kasihan, Bantul<br>
+              Yogyakarta 55183<br>
+              <a href="https://goo.gl/MmskEh">Lihat di Peta Google</a>
+            </section>
+        
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
+
+  <!-- Copyright -->
+  <div class="footer-copyright text-center py-3">Copyright &copy;<script>document.write(new Date().getFullYear());</script> Smatecin All rights reserved
+  </div>
+  <!-- Copyright -->
+
+</footer>
+        
+  <!-- ================ End footer Area ================= -->
+  <script src="vendors/jquery/jquery-3.2.1.min.js"></script>
+  <script src="vendors/bootstrap/bootstrap.bundle.min.js"></script>
+  <script src="vendors/owl-carousel/owl.carousel.min.js"></script>
+  <script src="js/jquery.ajaxchimp.min.js"></script>
+  <script src="js/mail-script.js"></script>
+  <script src="js/main.js"></script>
+  <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+  <script>
+  AOS.init();
+</script>
+</body>
+</html>
+
